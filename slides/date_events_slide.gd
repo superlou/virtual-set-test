0fba94a9a72f749da9a936da57f67e99580a90ce
superlou <lousimons@gmail.com>
2025-02-24 21:37:02 -0500

    Added stuff

--- slides/date_events_slide.gd
+++ slides/date_events_slide.gd
@@ -5,6 +5,9 @@ class_name DateEventsSlide
 
 @export var date = "1/1/25"
 @export var lmc_events:Array[LMCEvent]
+@export var date_text_color:Color
+@export var date_underline_color:Color
+@export var grid_text_color:Color
 
 @onready var info_grid:GridContainer = $InfoGrid
 
@@ -16,6 +19,7 @@ func _ready() -> void:
 	var weekday = DateUtil.humanize_weekday(date_dict.weekday)
 	
 	$DateLabel.text = "%s %s/%s" % [weekday, date_dict.month, date_dict.day]
+	$DateLabel.add_theme_color_override("font_color", date_text_color)
 
 	var font:Font = $DateLabel.get_theme_font("font")
 	var width = font.get_multiline_string_size(
@@ -23,6 +27,7 @@ func _ready() -> void:
 		$DateLabel.get_theme_font_size("font_size")
 	).x
 	$DateUnderline.size.x = width
+	$DateUnderline.color = date_underline_color
 
 	populate_events()
 
@@ -31,8 +36,10 @@ func populate_events() -> void:
 	for event in lmc_events:
 		var label := Label.new()		
 		label.text = event.time
+		label.add_theme_color_override("font_color", grid_text_color)
 		info_grid.add_child(label)
 
 		label = Label.new()
+		label.add_theme_color_override("font_color", grid_text_color)
 		label.text = event.title
 		info_grid.add_child(label)
